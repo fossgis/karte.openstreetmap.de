104f53c44a0262fedfb5f194c794797924992218
Jakob Miksch <info@jakobmiksch.eu>
2025-07-20 12:30:35 +0200

    Add basic MapLibre map

---
+++ public/index.js
@@ -0,0 +1,34 @@
+const style = {
+  version: 8,
+  sources: {
+    "osm-german-style": {
+      type: "raster",
+      tiles: ["https://tile.openstreetmap.de/{z}/{x}/{y}.png"],
+      tileSize: 256,
+      attribution: "Kartendaten von OpenStreetMap",
+    },
+  },
+  layers: [
+    {
+      id: "osm-german-style-layer",
+      type: "raster",
+      source: "osm-german-style",
+      minzoom: 0,
+      maxzoom: 22,
+    },
+  ],
+};
+
+const boundsGermany = [
+  [5, 47],
+  [16, 56],
+];
+
+new maplibregl.Map({
+  container: "map",
+  bounds: boundsGermany,
+  style: style,
+  maplibreLogo: false,
+  dragRotate: false,
+  touchZoomRotate: false,
+});
