1d3543f7a20f246fb0283b03cfd63f1a0ae326c6
astridx <info@astrid-guenther.de>
2025-10-02 13:52:10 +0200

    simplify query

--- public/lib/intern/osm-de-geocoder.js
+++ public/lib/intern/osm-de-geocoder.js
@@ -3,14 +3,11 @@ export function createGeocoderControl() {
     forwardGeocode: async (config) => {
       const features = [];
       try {
-        const request = `https://nominatim.openstreetmap.org/search?q=${config.query}&format=geojson&polygon_geojson=1&addressdetails=1`;
+        const request = `https://nominatim.openstreetmap.org/search?q=${config.query}&format=geojson`;
         const response = await fetch(request);
         const geojson = await response.json();
         for (const feature of geojson.features) {
-          const center = [
-            feature.bbox[0] + (feature.bbox[2] - feature.bbox[0]) / 2,
-            feature.bbox[1] + (feature.bbox[3] - feature.bbox[1]) / 2,
-          ];
+          const center = [feature.geometry.coordinates[0], feature.geometry.coordinates[1]];
           const point = {
             type: "Feature",
             geometry: {
