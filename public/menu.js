a2016c7ab4390d5ddeacec8b776922ebfa8e166b
Jakob Miksch <info@jakobmiksch.eu>
2026-01-03 00:05:30 +0100

    Hide menus on click outside

---
+++ public/menu.js
@@ -0,0 +1,22 @@
+/** 
+ * Close dropdown menus when clicked outside of it
+ */
+const closeDropdown = (event) => {
+  const { target } = event;
+
+  const dropDownMenus = document.querySelectorAll(".dropdown");
+
+  dropDownMenus.forEach((dropdown) => {
+    // return if clicked on dropdonw itself
+    if (dropdown.contains(target)) {
+      return;
+    }
+
+    // find hidden checkbox that controls visibility and uncheck if needed
+    const hiddenCheckbox = dropdown.getElementsByTagName("input")[0]
+    if (hiddenCheckbox) hiddenCheckbox.checked = false;
+  });
+};
+
+// activate listener
+document.addEventListener("click", closeDropdown);
