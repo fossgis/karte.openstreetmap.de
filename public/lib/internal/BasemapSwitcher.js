95dfcaca1c9e19d3eda58b24cf0dda2343b6a7bf
Jakob Miksch <info@jakobmiksch.eu>
2026-02-21 13:06:57 +0100

    fix: closing basemap switcher works again (#70)

--- public/lib/internal/BasemapSwitcher.js
+++ public/lib/internal/BasemapSwitcher.js
@@ -5,6 +5,7 @@ export class BasemapSwitcher {
   constructor(basemapConfig) {
     this._basemapConfig = basemapConfig;
     this._LAYER_PARAM_NAME = "layer";
+    this._CONTROL_BUTTON_CONTENT_ID = "basemap-switcher-toggle-button";
   }
 
   onAdd(map) {
@@ -48,7 +49,17 @@ export class BasemapSwitcher {
     });
 
     // hide basemap cards, when user clicks anywhere
-    document.addEventListener("pointerdown", () => {
+    document.addEventListener("pointerdown", (event) => {
+      const { target } = event;
+
+      // prevent hiding when main control button is hit
+      const controlButtonHit = target.matches(
+        "#" + this._CONTROL_BUTTON_CONTENT_ID,
+      );
+      if (controlButtonHit) {
+        return;
+      }
+
       basemapOptions.classList.add("hidden");
     });
 
@@ -78,6 +89,7 @@ export class BasemapSwitcher {
     });
 
     const btnContent = document.createElement("span");
+    btnContent.id = this._CONTROL_BUTTON_CONTENT_ID;
     btnContent.classList.add("maplibregl-ctrl-icon", "basemap-switcher-icon");
 
     outerButtonDiv.appendChild(innerButtonDiv);
